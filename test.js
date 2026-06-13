f9ce5b44507e0e036cecd80f1bd37647b63ca0d5
amikd <gvozd777dunaev@yandex.ru>
2026-06-13 15:40:59 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1932,36 +1932,6 @@
 
     function startPlugin() {
         window.lampac_plugin_ready = true;
-        if (!window.plugin_lampac_ready && !window.plugin_iptv_ready2) {
-            Lampa.SettingsApi.addComponent({
-                component: 'lampac',
-                icon: "<svg height=\"36\" viewBox=\"0 0 38 36\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n                <rect x=\"2\" y=\"8\" width=\"34\" height=\"21\" rx=\"3\" stroke=\"white\" stroke-width=\"3\"/>\n                <line x1=\"13.0925\" y1=\"2.34874\" x2=\"16.3487\" y2=\"6.90754\" stroke=\"white\" stroke-width=\"3\" stroke-linecap=\"round\"/>\n                <line x1=\"1.5\" y1=\"-1.5\" x2=\"9.31665\" y2=\"-1.5\" transform=\"matrix(-0.757816 0.652468 0.652468 0.757816 26.197 2)\" stroke=\"white\" stroke-width=\"3\" stroke-linecap=\"round\"/>\n                <line x1=\"9.5\" y1=\"34.5\" x2=\"29.5\" y2=\"34.5\" stroke=\"white\" stroke-width=\"3\" stroke-linecap=\"round\"/>\n            </svg>",
-                name: 'Lampac'
-            });
-            var uniqs = unic_id.slice(-3).toUpperCase();
-            Lampa.SettingsApi.addParam({
-                component: 'lampac',
-                param: {
-                    name: 'only_title',
-                    type: 'title',
-                    default: true
-                },
-                field: {
-                    name: 'Код устройства ' + uniqs
-                }
-            });
-            Lampa.SettingsApi.addParam({
-                component: 'lampac',
-                param: {
-                    name: 'only_title',
-                    type: 'title',
-                    default: true
-                },
-                field: {
-                    name: 'Онлайн'
-                }
-            });
-        }
         var manifest = {
             type: 'video',
             version: '',
